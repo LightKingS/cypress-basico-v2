6c78472267cce34709b81cf87d8fb325da033ae1
André Reis da Luz <Andr Luz>
2022-08-24 12:09:07 -0300

    Envia novos exercícios e otimiza anteriores

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -12,13 +12,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .type('André')
 
         cy.get('input[id="lastName"]')
-        .type('Reis da Luz')
+        .type('Reis da Luz', { delay: 0 })
 
         cy.get('input[id="email"]')
-        .type('andrereisdaluz@hotmail.com')
+        .type('andrereisdaluz@hotmail.com', { delay: 0 })
 
         cy.get('textarea[id="open-text-area"]')
-        .type('Olá estou mandando aqui uma mensagem pipipi popopo')
+        .type('Olá estou mandando aqui uma mensagem pipipi popopo', { delay: 0 })
 
         cy.get('button[type="submit"]')
         .click()
@@ -32,13 +32,13 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .type('André')
 
         cy.get('input[id="lastName"]')
-        .type('Reis da Luz')
+        .type('Reis da Luz', { delay: 0 })
 
         cy.get('input[id="email"]')
-        .type('andrereisdaluz@hotmail,com')
+        .type('andrereisdaluz@hotmail,com', { delay: 0 })
 
         cy.get('textarea[id="open-text-area"]')
-        .type('Olá estou mandando aqui uma mensagem pipipi popopo')
+        .type('Olá estou mandando aqui uma mensagem pipipi popopo', { delay: 0 })
 
         cy.get('button[type="submit"]')
         .click()
@@ -47,7 +47,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('verifica se ao digitar texto na caixa de telefone, a caixa continua vazia', () => {
-        cy.get('#phone').type('qweutoikjashdmnzxc').should('have.value', '')
+        cy.get('#phone').type('qweutoikjashdmnzxc', { delay: 0 }).should('have.value', '')
     })
 
     it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', ()=> {
@@ -55,15 +55,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .type('André')
 
         cy.get('input[id="lastName"]')
-        .type('Reis da Luz')
+        .type('Reis da Luz', { delay: 0 })
 
         cy.get('input[id="email"]')
-        .type('andrereisdaluz@hotmail.com')
+        .type('andrereisdaluz@hotmail.com', { delay: 0 })
 
         cy.get('textarea[id="open-text-area"]')
-        .type('Olá estou mandando aqui uma mensagem pipipi popopo')
+        .type('Olá estou mandando aqui uma mensagem pipipi popopo', { delay: 0 })
 
-        cy.get('#phone-checkbox').click()
+        cy.get('#phone-checkbox').check()
         
         cy.get('button[type="submit"]').click()
 
@@ -76,11 +76,11 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .clear().should('have.value', '')
 
         cy.get('input[id="lastName"]')
-        .type('Reis da Luz').should('have.value', 'Reis da Luz')
+        .type('Reis da Luz', { delay: 0 }).should('have.value', 'Reis da Luz')
         .clear().should('have.value', '')
 
         cy.get('input[id="email"]')
-        .type('andrereisdaluz@hotmail.com').should('have.value', 'andrereisdaluz@hotmail.com')
+        .type('andrereisdaluz@hotmail.com', { delay: 0 }).should('have.value', 'andrereisdaluz@hotmail.com')
         .clear().should('have.value', '')
 
         cy.get('#phone').type('364723').should('have.value', '364723')
@@ -95,5 +95,52 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     it('envia o formuário com sucesso usando um comando customizado', () => {
         cy.fillMandatoryFieldsAndSubmit()
+    })
+
+    it('seleciona um produto (youtube) pelo seu texto', () => {
+        cy.get('#product').select('YouTube').should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (mentoria) pelo seu valor', () => {
+        cy.get('#product').select('mentoria').should('have.value', 'mentoria')
+    })
+
+    it('seleciona um produto (blog) pelo seu indice', () => {
+        cy.get('#product').select(1).should('have.value', 'blog')
+    })
+
+    it('marca o tipo de atendimento "Feedback"', () => {
+        cy.get('input[value="feedback"]').check().should('be.checked')
+    })
+
+    it('marca cada tipo de atendimento', () => {
+        cy.get('input[type="radio"]').each((input) => {
+            cy.wrap(input).check().should('be.checked') 
+        })
+    })
+
+    it('marca ambos checkboxes, depois desmarca o último', () => {
+        cy.get('input[type="checkbox"]').check().should('be.checked')
+        cy.get('input[type="checkbox"]').last().uncheck().should('not.be.checked')
+    })
+
+    it('seleciona um arquivo da pasta fixtures', () => {
+        cy.get('#file-upload').selectFile('cypress/fixtures/example.json').should(($input) => {
+            //console.log($input)
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+
+    it('seleciona um arquivo simulando um drag-and-drop', () => {
+        cy.get('#file-upload').selectFile('cypress/fixtures/example.json', { action: 'drag-drop' }).should(($input) => {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
+    it('seleciona um arquivo simulando um drag-and-drop', () => {
+        cy.fixture('example.json', { encoding: null }).as('myFixture')
+        cy.get('#file-upload').selectFile('@myFixture').should(($input) => {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+
     })
 })
